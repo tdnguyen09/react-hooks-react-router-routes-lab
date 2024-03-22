9a5d05178d24133a9d31a10204221e4eff18db8f
Thuy Duong Nguyen <thuyduong.nguyen.091096@gmail.com>
2024-03-22 16:33:34 +1100

    complete lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => {
+        return (
+          <div key={index}>
+            <h2>{actor.name}</h2>
+            <p>Movies</p>
+            <ul>
+              {actor.movies.map((movie, i) => (
+                <li key={i}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director,index) => {
+        return (
+          <div key={index}>
+            <h2>{director.name}</h2>
+            <p>Movies</p>
+            <ul>
+              {director.movies.map((movie, i) => (
+                <li key={i}>{movie}</li>
+              ))}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Directors;
